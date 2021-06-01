b7f823f6e98ff69d6557454d700ec6cfda95d0c8
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-01 11:41:34 -0300

    Adding html and css files to the project and returning them when there are requests

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const adminRoutes = require('./routes/admin');
@@ -7,8 +9,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000); 
