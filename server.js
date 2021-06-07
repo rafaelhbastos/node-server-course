406a4885ca8eceb908047baa5d233369ff7defaa
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-07 14:36:28 -0300

    adding ejs files to the project and learning how to deal with it

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const path = require('path');
 
 const express = require('express');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views98uy -======= ','views');
 
 app.use(express.json());
@@ -20,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found'});
+    res.status(404).render('404', {pageTitle: 'Page not Found', path: ''});
 });
 
 app.listen(3000); 
