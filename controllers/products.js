784e7d3b53dd600bcdb13c59600c00a3015e113e
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-09 11:26:19 -0300

    implementing models into our project

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -9,15 +9,19 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    products: products, 
-    pageTitle: 'Shop', 
-    path: '/', 
-    hasProducts: products.length > 0, 
-    activeShop: true});
+  const products = Product.fetchAll(products => {
+    res.render('shop', {
+      products: products, 
+      pageTitle: 'Shop', 
+      path: '/', 
+      hasProducts: products.length > 0, 
+      activeShop: true
+    });
+  });
 };
