747fb269efebe12da0c5ae71ec1c5857d03de0e1
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-06-21 11:45:06 -0300

    cart model added, ejs files for detail added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   const products = Product.fetchAll(products => {
@@ -8,6 +9,17 @@ exports.getProducts = (req, res, next) => {
       path: '/products', 
       hasProducts: products.length > 0, 
       activeShop: true
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product, 
+      pageTitle: product.title,
+      path: '/products'
     });
   });
 };
@@ -26,6 +38,21 @@ exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
   });
 };
 
