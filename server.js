c1fdd3a8058a87e10f9428a222484192f47e8f3e
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-07-12 09:46:44 -0300

    refactoring putting users in some functions

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -17,13 +19,33 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-  
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+sequelize.sync().then(() => {
+  return User.findByPk(1)
+})
+.then(user => {
+  if(!user) {
+    return User.create({name: 'Rafael', email:'test@test.com'});
+  }
+  return user;
+})
+.then(() => {
   app.listen(3000); 
 })
 .catch(err => console.log(err));
