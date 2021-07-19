00fe015a6f55696324bd1321522a5ecb860375ad
rafaelhbastos <rafaelhenribastos12@gmail.com>
2021-07-19 11:07:10 -0300

    Updating products with mongodb done

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description,
-  })
+  const product = new Product(title, price, description, imageUrl);
+  product.save()
   .then(result => {
     console.log('Product Created');
     res.redirect('/admin/products');
@@ -32,10 +28,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user.getProducts({ where: { id: prodId } })
+  Product.findById(prodId)
   // Product.findByPk(prodId)
-  .then(products => {
-    const product = products[0];
+  .then(product => {
     if(!product) {
       return res.redirect('/')
     }
@@ -55,43 +50,37 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDescription = req.body.description;
-  Product.findByPk(prodId)
-  .then(product => {
-    product.title = updatedTitle;
-    product.price = updatedPrice;
-    product.description = updatedDescription;
-    product.imageUrl = updatedImageUrl;
-    return product.save();
-  })
-  .then(() => {
-    console.log('Updated Product');
-    res.redirect('/admin/products');
-  })
-  .catch(err => console.log(err));
+    const product = new Product(updatedTitle, updatedPrice, updatedDescription, updatedImageUrl, prodId);
+    product.save()
+      .then(() => {
+        console.log('Updated Product');
+        res.redirect('/admin/products');
+      })
+      .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  req.user.getProducts()
-  .then(products => {
-    res.render('admin/products', {
-      products: products, 
-      pageTitle: 'Admin Porducts', 
-      path: '/admin/products', 
-      hasProducts: products.length > 0, 
-      activeShop: true
-    });
-  }).catch(err => console.log(err));
+  Product.fetchAll()
+    .then(products => {
+      res.render('admin/products', {
+        products: products, 
+        pageTitle: 'Admin Porducts', 
+        path: '/admin/products', 
+        hasProducts: products.length > 0, 
+        activeShop: true
+      });
+    }).catch(err => console.log(err));
 }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product => {
-      return product.destroy();
-    })
-    .then(() => {
-      console.log('Product destroyed');
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
-};
+// exports.postDeleteProduct = (req, res, next) => {
+//   const prodId = req.body.productId;
+//   Product.findByPk(prodId)
+//     .then(product => {
+//       return product.destroy();
+//     })
+//     .then(() => {
+//       console.log('Product destroyed');
+//       res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(err));
+// };
--- util/database.js
+++ util/database.js
@@ -1,5 +1,29 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node_course', 'root', process.env.MySQL_PASSWORD, {dialect: 'mysql', host: 'localhost'});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://rafaelpessoa12:' + process.env.MONGO_ATLAS_PW + '@cluster0.05jl7.mongodb.net/node_course?retryWrites=true&w=majority')
+  .then(client => {
+    console.log('Connected');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log(err)
+    throw err;
+  });
+};
+
+const getDb = () => {
+  if(_db) {
+    return _db;
+  }
+  throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb;
+
+
